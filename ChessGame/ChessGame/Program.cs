8bec74a1d92c0cfce17b1db9deefd88d71151b64
virgiliobreda <virgiliobred031@gmail.com>
2023-11-24 11:57:44 -0300

    Adding base folder and position class

---
+++ ChessGame/ChessGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using ChessGame.Board;
+
+namespace ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Position p = new Position(3, 4);
+            Console.WriteLine($"Position: {p}");
+
+
+        }
+    }
+}
